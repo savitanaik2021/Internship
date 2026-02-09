bd4ed7db06aca0702fbaf0f4df807cd38322551a
savita <savitaramesh2021@gmail.com>
2026-02-09 21:25:55 +0530

    OOP

---
+++ Day 8/Decorator.py
@@ -0,0 +1,14 @@
+def admin_only(func):
+    def wrapper(username):
+        if username == "admin":
+            func(username)
+        else:
+            print("Access Denied")
+    return wrapper
+
+@admin_only
+def dashboard(username):
+    print("Welcome to Admin Dashboard")
+
+dashboard("admin")   
+dashboard("user")    
